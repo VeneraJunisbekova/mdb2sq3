d44809717e9e43bbea342f0f3809c728804c6c13
Andi Palo <sanandrea8080@gmail.com>
2015-10-30 15:07:58 +0100

    initial commit

---
+++ Test/UtilsTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace mdb2sq3.Tests
+{
+    class UtilsTests
+    {
+        public static void RunTableFindTest()
+        {
+            PrintBeginTest("Find Full Table");
+            string testQuery = "I am (leg (asdf) end) RIGHT JOIN some other text";
+
+            Tuple<int,int> result = Utils.FindFullTable(testQuery, 4, false);
+            string table = testQuery.Substring(result.Item1, result.Item2 - result.Item1);
+            Console.WriteLine(table);
+            Debug.Assert(table.Equals("(leg (asdf) end)"),table);
+
+            result = Utils.FindFullTable(testQuery, 22, true);
+            table = testQuery.Substring(result.Item2, result.Item1 - result.Item2);
+            Console.WriteLine(table);
+            Debug.Assert(table.Equals("(leg (asdf) end)"), table);
+
+            result = Utils.FindFullTable(testQuery, 21, false);
+            table = testQuery.Substring(result.Item1, result.Item2 - result.Item1);
+            Console.WriteLine(table);
+            Debug.Assert(table.Equals("RIGHT"), table);
+
+            result = Utils.FindFullTable(testQuery, 28, true);
+            table = testQuery.Substring(result.Item2, result.Item1 - result.Item2);
+            Console.WriteLine(table);
+            Debug.Assert(table.Equals("RIGHT"), table);
+
+            PrintEndTest("Find Composite");
+        }
+
+        public static void RunSwapRightJoinTest()
+        {
+            PrintBeginTest("Swap");
+            string input = "The following test will (do a now RIGHT JOIN back) RIGHT JOIN outer table";
+            string test = "The following test will outer LEFT JOIN (do a back LEFT JOIN now) table";
+            string output = Utils.SwapTablesOfRightJoin(input);
+            Console.Out.WriteLine(input);
+            Console.Out.WriteLine(output);
+
+            Debug.Assert(output.Equals(test));
+
+            PrintEndTest("Swap");
+        }
+
+        private static void PrintBeginTest(string name)
+        {
+            Console.WriteLine("---------------  Begin Test " + name + "-------------------");
+        }
+        private static void PrintEndTest(string name)
+        {
+            Console.WriteLine("_______________  End Test " + name + " ____________________\n\n");
+        }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace mdb2sq3
+{
+    class Utils
+    {
+        public static string SwapTablesOfRightJoin(string query)
+        {
+            string keyword = "right join";
+            int index = query.IndexOf(keyword, StringComparison.OrdinalIgnoreCase);
+            //Console.Out.WriteLine("Index is: " + index);
+            if (index > 0)
+            {
+                Tuple<int, int> prev = FindFullTable(query, index, true);
+                Tuple<int, int> next = FindFullTable(query, index + keyword.Length, false);
+
+                string firstTable = query.Substring(prev.Item2, prev.Item1 - prev.Item2);
+                string secondTable = query.Substring(next.Item1, next.Item2 - next.Item1);
+
+                //Console.Out.WriteLine("Prev: " + prev.Item1 + "-" + prev.Item2 + " Next: " + next.Item1 + "-" + next.Item2);
+                //Console.Out.WriteLine(firstTable + "|");
+                //Console.Out.WriteLine(secondTable + "|");
+
+                StringBuilder strBuilder = new StringBuilder(query);
+
+                string newJoin = secondTable + " LEFT JOIN " + firstTable;
+
+                strBuilder.Remove(prev.Item2, next.Item2 - prev.Item2);
+                //Console.WriteLine("After remove: " + strBuilder.ToString());
+                strBuilder.Insert(prev.Item2, newJoin);
+                return SwapTablesOfRightJoin(strBuilder.ToString());
+            }
+            return query;
+        }
+
+
+        public static Tuple<int, int> FindFullTable(string query, int index, bool isBefore)
+        {
+            char c;
+            bool isInside = false;
+            bool isComposite = false;
+            int counter = 0;
+
+            int beginIndex = index;
+            int endIndex = beginIndex;
+
+            char beginChar = (isBefore) ? ')' : '(';
+            char endChar = (isBefore) ? '(' : ')';
+
+            while (true)
+            {
+                c = (isBefore) ? query[--endIndex] : query[++endIndex];
+                if (c == beginChar) 
+                { 
+                    isComposite = true;
+                    counter++;
+                }
+
+                if (c == endChar)
+                {
+                    counter--;
+                    if (counter <= 0)
+                    {
+                        if (!isBefore && isComposite)
+                            endIndex++;
+                        break;
+                    }
+                }
+                if (c != ' ')
+                {
+                    if (!isInside)
+                    {
+                        isInside = true;
+                        beginIndex = endIndex + (isBefore ? + 1 : 0);
+                    }
+                }
+                else
+                {
+                    if (isInside && !isComposite)
+                    {
+                        if (isBefore)
+                            endIndex++;
+                        break;
+                    }
+                }
+            }
+            
+            return Tuple.Create(beginIndex,endIndex);
+        }
+    }
+}
